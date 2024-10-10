d29cfa047e72c8ffac1abe1da915b4714f80bd05
sitisija <Siti.SIJA22@gmail.com>
2024-10-10 09:18:42 +0700

    menampilkan slider

--- app/Http/Controllers/backend/SliderController.php
+++ app/Http/Controllers/backend/SliderController.php
@@ -4,10 +4,14 @@ namespace App\Http\Controllers\backend;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Sliders;
 
 class SliderController extends Controller
 {
     public function index(){
-        return view('backend.slider.index');
+        $sliders=Sliders::get();
+        return view('backend.sliders.index',[
+            'sliders'=>$sliders
+        ]);
     }
 }
---
+++ resources/views/backend/sliders/index.blade.php
@@ -0,0 +1,39 @@
+@extends('backend.layout.master')
+@section('content')
+    <div class="card shadow mb-4">
+        <div class="card-header py-3">
+            <h6 class="m-0 font-weight-bold text-primary">Slider</h6>
+        </div>
+        <div class="card-body">
+            <a href="" class="btn btn-primary mb-2">Tambah slider</a>
+            <div class="table-responsive">
+                <table class="table" id="dataTable" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Judul</th>
+                            <th>File</th>
+                            <th>Deskripsi</th>
+                            <th>Aksi</th>
+                        </tr>
+                    </thead>
+                      @foreach ($sliders as $item)
+                      <tr>
+                          <td>{{$item->id}}</td>
+                          <td>{{$item->title}}</td>
+                          <td><img src="{{asset($item->file)}}" width="200" alt="images"></td>
+                          <td>{{$item->description}}</td>
+                          <td><a href="" class="btn btn-warning">edit</a>
+                            <form action="" method="post">
+                                @csrf
+                                <button class="btn btn-danger">Hapus</button>
+                            </form>
+                        </td>
+                        </tr>
+                    </tbody>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
